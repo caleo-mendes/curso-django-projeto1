5dcf3ba46953163fdca166a7715363d283a61bf4
caleo-mendes <caleomendes@yahoo.com.br>
2022-12-30 18:23:35 -0300

    views e urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import path
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def contact(request):
+    return HttpResponse('Contato')
+
+
+def about(request):
+    return HttpResponse('Sobre')
