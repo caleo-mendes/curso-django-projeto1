40e19126bbf57368b302bd1eeed4fffe574a0f06
caleo-mendes <caleomendes@yahoo.com.br>
2022-12-30 19:25:48 -0300

    Exclusão Contato e  Sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from recipes.views import about, contact, home
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('sobre/', about),
-    path('contato/', contact),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contact(request):
-    return render(request, 'recipes/contato.html')
-
-
-def about(request):
-    return render(request, 'recipes/sobre.html')
